bbc73ca9be2e91aad14c9a1b5a1f47dacaf2d6b5
张珂 <609975727@qq.com>
2016-11-07 17:23:41 +0800

    Update PageDetail3Activity.java

--- app/src/main/java/com/mrzk/example/pagelist/PageDetail3Activity.java
+++ app/src/main/java/com/mrzk/example/pagelist/PageDetail3Activity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
